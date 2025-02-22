30a7d623781af2003752072b63230776d5491d1e
starbachi <eelikg1916@outlook.com>
2025-02-22 17:04:35 +0000

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import postcss from "postcss";
+import tailwindcss from "@tailwindcss/postcss";
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+      ],
+    },
+  },
 });
